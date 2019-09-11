ef9b0d9e86a523e899b48de23522eedc1e9b76e4
SINGER Louis <louis.singer@efrei.net>
2019-09-11 10:22:40 +0200

    add stop subject in deleteComponent

--- src/app/delete-card/delete-card.component.ts
+++ src/app/delete-card/delete-card.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { SnackbarService } from '../core/snackbar.service';
 import { BooksService } from '../core/services/books.service';
 import { Book } from '../model/book';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-delete-card',
   templateUrl: './delete-card.component.html',
   styleUrls: ['./delete-card.component.css']
 })
-export class DeleteCardComponent implements OnInit {
+export class DeleteCardComponent implements OnInit, OnDestroy {
 
   private formGroup: FormGroup;
+
+  private stop = new Subject();
 
   constructor(
     private formBuilder: FormBuilder, 
@@ -31,7 +35,12 @@ export class DeleteCardComponent implements OnInit {
 
   deleteBook() {
     this.bookService.delete(this.formGroup.get('id').value)
+    .pipe(takeUntil(this.stop))
     .subscribe((book: Book) => this.snackbarService.show('The book ' + book.title + ' has been deleted.'));
   }
 
+  ngOnDestroy() {
+    this.stop.next();
+    this.stop.complete();
+  }
 }
