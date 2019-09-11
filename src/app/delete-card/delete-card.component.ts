fba6d25a5f17f03e35807019ea9847f073d5f731
SINGER Louis <louis.singer@efrei.net>
2019-09-11 10:20:32 +0200

    delete card done

--- src/app/delete-card/delete-card.component.ts
+++ src/app/delete-card/delete-card.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { SnackbarService } from '../core/snackbar.service';
+import { BooksService } from '../core/services/books.service';
+import { Book } from '../model/book';
 
 @Component({
   selector: 'app-delete-card',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteCardComponent implements OnInit {
 
-  constructor() { }
+  private formGroup: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder, 
+    private snackbarService: SnackbarService,
+    private bookService: BooksService
+  ) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  private createForm() {
+    this.formGroup = this.formBuilder.group({
+      id: [null, Validators.required],
+    });
+  }
+
+  deleteBook() {
+    this.bookService.delete(this.formGroup.get('id').value)
+    .subscribe((book: Book) => this.snackbarService.show('The book ' + book.title + ' has been deleted.'));
   }
 
 }
